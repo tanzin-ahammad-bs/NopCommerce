9c3d088bc4b0daadd19d654725e30fb6da192243
tanzin-ahammad-bs <tanzin.ahammad@brainstation-23.com>
2025-04-09 18:06:47 +0600

    Created a widget named: "MyPlugin".

---
+++ src/Plugins/Nop.Plugin.Widget.HelloWorld/Components/ExampleWidgetViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+
+
+namespace Nop.Plugin.Widgets.HelloWorld.Components
+{
+    public class ExampleWidgetViewComponent : NopViewComponent
+    {
+        public IViewComponentResult Invoke(string widgetZone, object additionalData)
+        {
+            return Content("Hello World");
+        }
+    }
+}
